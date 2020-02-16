3d5688acf15e47a7c90bca212a1f15c829574471
Bruno Lacerda <brunolcpg@gmail.com>
2020-02-15 22:38:56 -0300

    Add prettier rules and prettierrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,10 @@ module.exports = {
     node: true,
   },
   extends: [
-    'airbnb-base',
+    'airbnb-base', 'prettier',
+  ],
+  plugins: [
+    'prettier'
   ],
   globals: {
     Atomics: 'readonly',
@@ -15,6 +18,7 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    "prettier/prettier": "error",
     "class-methods-use-this": "off",
     "no-param-reassign": "off",
     "camelcase": "off",
