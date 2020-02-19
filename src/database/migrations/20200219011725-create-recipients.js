cd6120d649fdf9a35c191abb026692754299cd7a
Bruno Lacerda <brunolcpg@gmail.com>
2020-02-18 22:47:31 -0300

    Add timestamps at recipient's table

--- src/database/migrations/20200219011725-create-recipients.js
+++ src/database/migrations/20200219011725-create-recipients.js
@@ -35,6 +35,12 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
+      created_at: {
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+      },
     });
   },
 
