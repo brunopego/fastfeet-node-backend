f40fb911fdf44a269e78b00f51eeff908a1d60f2
Bruno Lacerda <brunolcpg@gmail.com>
2020-02-18 22:49:44 -0300

    Add recipients creation route

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import RecipientController from './app/controllers/RecipientController';
+
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -12,5 +14,6 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware); // The routes bellow will use this middleware
 
 routes.put('/users', UserController.update);
+routes.post('/recipients', RecipientController.store);
 
 export default routes;
